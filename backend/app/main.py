f9d2b0a797d62d1d3a5adbc67fe7e4c1007acddf
Céline <dorignak@gmail.com>
2026-03-28 16:08:07 +0100

    create new api get one product (#15)

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
+
 from typing import Optional
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app import models
@@ -18,3 +19,10 @@ def get_products(
     if in_stock:
         query = query.filter(models.Product.stock > 0)
     return query.all()
+
+@app.get("/products/{product_id}")
+def get_product(product_id: int, db: Session = Depends(get_db)):
+    product = db.query(models.Product).filter(models.Product.id == product_id).first()
+    if product is None:
+        raise HTTPException(status_code=404, detail=f"Product with id {product_id} not found")
+    return product
