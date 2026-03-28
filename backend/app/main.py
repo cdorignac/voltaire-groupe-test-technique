f9e8dcf38b17c6e127621cea6b0b55599e380c38
Céline <dorignak@gmail.com>
2026-03-28 15:55:43 +0100

    create new api get all products (#14)

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app import models
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/products")
+def get_products(
+    category: Optional[str] = None, 
+    in_stock: bool = False,
+    db: Session = Depends(get_db)
+):
+    query = db.query(models.Product)
+    if category:
+        query = query.filter(models.Product.category == category)
+    if in_stock:
+        query = query.filter(models.Product.stock > 0)
+    return query.all()
