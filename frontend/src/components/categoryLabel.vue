6847665a17aadb8839629798946ed116dafc054e
celine.dorignac <celine.dorignac@euranova.eu>
2026-03-30 18:34:17 +0200

    create new page with all products

---
+++ frontend/src/components/categoryLabel.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { Category } from "@/models/product"
+import { computed } from "vue"
+
+const props = defineProps<{
+  category: Category
+}>()
+
+const categoryColor = computed(() => {
+  switch (props.category) {
+    case Category.Accessory:
+      return 'secondary'
+    case Category.Saddle:
+      return 'info'
+    case Category.Stirrups:
+      return 'accent'
+    default:
+      return 'grey'
+  }
+})
+
+const categoryLabel = computed(() => {
+  switch (props.category) {
+    case Category.Accessory:
+      return 'Accessory'
+    case Category.Saddle:
+      return 'Saddle'
+    case Category.Stirrups:
+      return 'Stirrups'
+    default:
+      return props.category
+  }
+})
+</script>
+
+<template>
+    <v-chip :color="categoryColor" class="ma-2"  size="small">
+      {{ categoryLabel }}
+    </v-chip>
+</template>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,9 +6,32 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// Vuetify
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { EquistockTheme } from './equistockTheme'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+  },
+  theme: {
+    defaultTheme: 'EquistockTheme',
+    themes: {
+      EquistockTheme,
+    },
+  },
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
