acc19200afcd4872d803a178c448810932e624d4
Céline <dorignak@gmail.com>
2026-03-31 21:14:33 +0200

    add api tests (#25)

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 import os
 
 # The 'db' hostname matches the service name in docker-compose.yml
